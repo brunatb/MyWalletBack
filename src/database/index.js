1151d695311f131355c4821ff1e315dcf32a9dce
brunatb <brutopanb@gmail.com>
2021-01-29 13:02:41 -0300

    Change database connection

--- src/database/index.js
+++ src/database/index.js
@@ -3,6 +3,9 @@ const { Pool } = require('pg');
 
 const connection = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = connection;
